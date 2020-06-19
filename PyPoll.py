7bb53477d79f29c97ea3f8b01122feec2956c96c
Steven <nicholssteven1@gmail.com>
2020-06-18 22:40:29 -0500

    adding election audit code error with printing wrong name in winner result

--- PyPoll.py
+++ PyPoll.py
@@ -5,12 +5,25 @@
 #  4. The total number of votes each candidate won
 #  5. The winner of the election based on popular vote.
 
+# dependants
 import csv
 import os
 # Assign a variable for the file to load and the path
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# initial vote counter
+total_votes = 0
+# pulling candidate options
+candidate_options = []
+# declare empty dictonary
+candidate_votes = {}
+# winner candi and count
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 
 # Open and read file
 with open(file_to_load) as election_data:
@@ -19,7 +32,57 @@ with open(file_to_load) as election_data:
         
         # Read and print header row
         headers = next(file_reader)
-        print(headers)
+
+        # print each row in the CSV file
+        for row in file_reader:
+                # add to total vote count
+                total_votes += 1
+                # print candidate name from each row
+                candidate_name = row[2]
+                
+                if candidate_name not in candidate_options:
+                        # add candidate name to list if no match exists
+                        candidate_options.append(candidate_name)
+
+                        # begin candidate vote tracking
+                        candidate_votes[candidate_name] = 0
+                # add vote to candi counb
+                candidate_votes[candidate_name] += 1
+        
+        # determine the % of votes for candies by looping thru count
+        for candidate in candidate_votes:
+                # retrieve vote count of a candi
+                votes = candidate_votes[candidate]
+                vote_percentage = float(votes) / float(total_votes) * 100
+                # print candi name and % + calc of percentage
+                # print(f"{candidate}: received {vote_percentage:.1f}% of the vote.")
+
+                # Determine winning vote count and candi
+                # Determine if the votes are greater than the winning count.
+                if (votes > winning_count) and (vote_percentage > winning_percentage):
+                # If true then set winning_count = votes and winning_percent = vote_percentage.
+                 winning_count = votes
+                 winning_percentage = vote_percentage
+                # Set the winning_candidate equal to the candidate's name.
+                winning_candidate = candidate 
+                print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+        winning_candidate_summary = (
+                 f"-------------------------\n"
+                 f"Winner: {winning_candidate}\n"
+                 f"Winning Vote Count: {winning_count:,}\n"
+                 f"Winning Percentage: {winning_percentage:.1f}%\n"
+                 f"-------------------------\n")
+        print(winning_candidate_summary)
+        # print winner candi,  vote, %
+        
+
+# # Print total votes.
+# print(total_votes)
+# # print candidate names
+# print(candidate_options)
+# # print candidate vote dict
+# print(candidate_votes)
+
 
 
 
